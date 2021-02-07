aff286e9091b2e6783b90ac0c65d440452e5591d
PropeReferio <lemuel.b.stevens@gmail.com>
2021-02-07 16:43:35 -0600

    Messing around with wordlists

--- try_words.py
+++ try_words.py
@@ -2,10 +2,12 @@ import requests
 import time
 
 start = time.time()
-with open('./wordlists/namelist.txt', 'r') as wordlist:
+
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0'}
+with open('./wordlists/real_paths.txt', 'r') as wordlist:
     for line in wordlist:
-        cur_url = f"https://facialabused.wordpress.com/{line}"
-        r = requests.get(cur_url)
+        cur_url = f"https://w3schools.com/{line}"
+        r = requests.get(cur_url, headers=headers)
         if r.status_code not in [400, 404]:
             print(f"Page found: {cur_url}\nStatus Code: {r.status_code}")
 
